5dd3b34d02fcaecaa9c5353a1593ef6443ea161b
lxst <lxst@YEGOR>
2025-09-16 20:33:09 +0500

    Добавьте файлы проекта.

---
+++ AnimalAbstract.cs
@@ -0,0 +1,67 @@
+﻿using Abstract_classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_classes
+{
+    abstract class AnimalAbstract
+    {
+        public abstract string Name { get; set; }
+
+        public AnimalAbstract(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+        }
+    }
+
+class Dog : AnimalAbstract
+{
+    private string name;
+    public override string Name 
+    { 
+        get => name; 
+        set => name = value; 
+    }
+
+    public Dog(string name)
+        : base(name)
+    {
+    }
+
+    public override void Say()
+    {
+        Console.WriteLine("Гав");
+    }
+}
+
+class Cat : AnimalAbstract
+{
+    private string name;
+    public override string Name
+    {
+        get => name;
+        set => name = value;
+    }
+
+    public Cat(string name)
+        : base(name)
+    {
+    }
+
+    public override void Say()
+    {
+        Console.WriteLine("Мяу");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AnimalAbstract dog = new Dog("Шарик");
+            AnimalAbstract cat = new Cat("Мурка");
+
+            dog.ShowInfo();
+            cat.ShowInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
